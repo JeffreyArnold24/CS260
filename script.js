3f4c1f39db26ac306ccd167d4a4ffd1db2aa180b
JeffreyArnold24 <90009267+JeffreyArnold24@users.noreply.github.com>
2021-10-26 23:19:22 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,95 @@
-function onClick(e) {
-  e.preventDefault();
-
-  let value = document.getElementById("selector").value;
-  console.log(value);
-
-  const url = "https://api.chucknorris.io/jokes/random";
-  //?category=" + value;
-
-      var http = new XMLHttpRequest();
-      var response = http.responseText;;
-
-      response = JSON.parse(response);
-      http.open ("GET", url, true);
-      http.send();
-      http.onreadystatechange = document.getElementById('joke').innerHTML = "<h1>" + response.value.joke + "</h1>";
-
-
-
-
-};
-
-document.getElementById('Go').addEventListener('click', onClick);
+let app = new Vue({
+  el: '#app',
+  data: {
+    number: '',
+    max:'',
+    current: {
+      title: '',
+      img: '',
+      alt: ''
+    },
+    loading: true,
+addedName: '',
+addedComment: '',
+comments: {},
+  },
+  created() {
+    this.xkcd();
+  },
+  methods: {
+    async xkcd() {
+  try {
+    this.loading = true;
+    let url = 'https://xkcd.now.sh/?comic=';
+    if (this.number === '') {
+      url += 'latest';
+    } else {
+      url += this.number;
+    }
+    const response = await axios.get(url);
+    this.current = response.data;
+    this.loading = false;
+    this.number = response.data.num;
+  } catch (error) {
+    this.number = this.max;
+  }
+},
+previousComic() {
+  this.number = this.current.num - 1;
+  if (this.number < 1)
+    this.number = 1;
+},
+nextComic() {
+  this.number = this.current.num + 1;
+  if (this.number > this.max)
+    this.number = this.max
+},
+getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive
+},
+randomComic() {
+  this.number = this.getRandom(1, this.max);
+},
+addComment() {
+  if (!(this.number in this.comments))
+    Vue.set(app.comments, this.number, new Array);
+  this.comments[this.number].push({
+    author: this.addedName,
+    text: this.addedComment
+  });
+  this.addedName = '';
+  this.addedComment = '';
+},
+},
+computed: {
+   month() {
+     var month = new Array;
+     if (this.current.month === undefined)
+       return '';
+     month[0] = "January";
+     month[1] = "February";
+     month[2] = "March";
+     month[3] = "April";
+     month[4] = "May";
+     month[5] = "June";
+     month[6] = "July";
+     month[7] = "August";
+     month[8] = "September";
+     month[9] = "October";
+     month[10] = "November";
+     month[11] = "December";
+     return month[this.current.month - 1];
+   }
+ },
+ watch: {
+   number(value, oldvalue) {
+     if (oldvalue === '') {
+       this.max = value;
+     } else {
+       this.xkcd();
+     }
+   },
+ },
+});
